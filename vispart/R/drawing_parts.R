1c8a4223f0e2082a8414e74070375ea0f5847a87
Salahub <cdsalahub@gmail.com>
2019-07-12 15:29:52 -0400

    Added header for all partitions function

--- vispart/R/drawing_parts.R
+++ vispart/R/drawing_parts.R
@@ -4,7 +4,7 @@
 ##' @param partition a partition represented as a sequence of positive integers
 ##' @param type one of "circle" or "rectangle"
 ##' @param eps a positive real number
-##' @param params a list of graphical paramters to be applied to the output
+##' @param params a list of graphical parameters to be applied to the output
 ##' @return a plot of the partition
 ##' @author Chris Salahub
 draw_part <- function(partition, type = c("circle","rectangle"), eps = 0.1,
@@ -13,3 +13,11 @@ draw_part <- function(partition, type = c("circle","rectangle"), eps = 0.1,
     rects <- part_coords(partition, type, eps, params) # create graphical object
     grid.draw(rects) # draw the object
 }
+
+##' @title Drawing All Partitions of n
+##' @param n a positive integer to be partitioned and displayed
+##' @param type one of "cicle" or "rectangle"
+##' @param eps a positive real number
+##' @param params a list of graphical parameters to be applied to the output
+##' @return a plot of all partitions of n
+##' @author Chris Salahub
