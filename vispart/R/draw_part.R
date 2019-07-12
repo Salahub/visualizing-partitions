6d99d31fb96b2279ff2393924875366404deaece
Salahub (WSL) <cdsalahub@gmail.com>
2019-07-12 08:43:29 -0400

    Small edits, fixed manual file name

--- vispart/R/draw_part.R
+++ vispart/R/draw_part.R
@@ -1,12 +1,14 @@
 ### The Draw Part Function ###############################################################
 
 ##' @title Drawing a Single Partition
-##' @param partition a partition of a number represented as a sequence of positive integers
+##' @param partition a partition represented as a sequence of positive integers
 ##' @param type one of "circle" or "rectangle"
 ##' @return a graphical object and a plot of the partition
 ##' @author Chris Salahub
 draw_partition <- function(n, type = c("circle","rectangle")) {
+    ## perform checks
     stopifnot(all(n >= 0)) # check the partition provided is valid
+    ## generate the coordinates
 }
 
-              
+
